f4258925fc7357aceead796249a5c21b459af971
DenisKostyukov <kostukov08@gmail.com>
2021-06-18 11:29:56 +0300

    fix localstorage

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 export function useTodo () {
   const [todo, setTodo] = useState(JSON.parse(localStorage.getItem('todo list')));
   useEffect(() => {
-    localStorage.setItem('todo list', JSON.stringify(todo));
+    localStorage.setItem('todo list', JSON.stringify(todo ? todo : []));
   }, [todo]);
   return { todo, setTodo };
 }
